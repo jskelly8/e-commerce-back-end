bb20405dc76661f425717beab14834341bc3becf
Jess Skelton <jessskelton888@gmail.com>
2024-01-24 12:31:22 -0600

    Worked with tutor to get started: Defined columns per assignment in Models folder for Category.js, Product.js, ProductTags.js and Tag.js & made necessary associations inside index.js --- Corrected server.js sequlize configuration and sync to database -- Began api routs inside category-routes.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
+// Associations
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category --- OneToMany
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// Categories have many Products --- OneToMany
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) --- ManyToMany
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
